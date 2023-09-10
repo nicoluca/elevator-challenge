73ea18242fb3fe08914949c3fa0d2cfe2d328c44
Nico <nico.pfau@icloud.com>
2023-09-10 15:56:16 +0200

    Parametrize tests

--- src/main/java/org/example/domain/ElevatorSystemImpl.java
+++ src/main/java/org/example/domain/ElevatorSystemImpl.java
@@ -34,7 +34,7 @@ public class ElevatorSystemImpl implements ElevatorSystem {
         // If there is an idle elevator, assign the request to it (move it to the origin floor, then to the destination floor)
         if (idleElevator.isPresent()) {
             logger.info("Found idle elevator " + idleElevator.get());
-            assignRequestToIdleElevator(elevatorRequest, idleElevator.get());
+            assignRequestToElevator(elevatorRequest, idleElevator.get());
             return;
         } else {
             logger.info("No idle elevator found");
@@ -42,14 +42,11 @@ public class ElevatorSystemImpl implements ElevatorSystem {
 
         // If there is no idle elevator, assign the request to the elevator that will be idle the soonest
         Elevator elevator = getNextIdleElevator();
-        assignRequestToIdleElevator(elevatorRequest, elevator);
+        assignRequestToElevator(elevatorRequest, elevator);
 
-        // If all elevators are busy, assign the request to the elevator that is closest to the request
-
-        // If all elevators are busy and are on the same distance from the request, assign the request to the elevator with the lowest id
     }
 
-    private void assignRequestToIdleElevator(ElevatorRequest elevatorRequest, Elevator elevator) {
+    private void assignRequestToElevator(ElevatorRequest elevatorRequest, Elevator elevator) {
         executorService.submit(
                 () -> {
                     elevator.move(elevatorRequest.getOriginFloor());
--- src/test/java/org/example/domain/ElevatorSystemImplTest.java
+++ src/test/java/org/example/domain/ElevatorSystemImplTest.java
@@ -3,6 +3,8 @@ package org.example.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,13 +23,22 @@ class ElevatorSystemImplTest {
         elevatorSystem.addRequest(new ElevatorRequest(1, 2, ElevatorState.UP));
     }
 
-    @Test
-    @DisplayName("Test addRequest for multiple requests")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5})
+    @DisplayName("Test for less requests than elevators")
     void testAddRequestForMultipleRequests() {
-        elevatorSystem.addRequest(new ElevatorRequest(1, 2, ElevatorState.UP));
-        elevatorSystem.addRequest(new ElevatorRequest(1, 3, ElevatorState.UP));
-        elevatorSystem.addRequest(new ElevatorRequest(1, 4, ElevatorState.UP));
-        elevatorSystem.addRequest(new ElevatorRequest(10, 5, ElevatorState.DOWN));
+        for (int i = 0; i < 5; i++) {
+            elevatorSystem.addRequest(new ElevatorRequest(1, 20, ElevatorState.UP));
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {7, 10, 15})
+    @DisplayName("Test for more requests than elevators")
+    void testForMultipleRequests(int numberOfRequests) {
+        for (int i = 0; i < numberOfRequests; i++) {
+            elevatorSystem.addRequest(new ElevatorRequest(1, 20, ElevatorState.UP));
+        }
     }
 
 }
